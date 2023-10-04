04f0b72eb2bba2f6eca0638b57ab574e32aa91a0
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2023-10-04 18:06:02 -0300

    inicio de la pagina 2

---
+++ TPE/pagina/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once './view/header.phtml';
+?>
+
+<h1>hola world</h1>
+
+<?php
+include_once './view/footer.phtml';
+
+?>
--- TPE/pagina/router.php
+++ TPE/pagina/router.php
@@ -1,2 +1,34 @@
 <?php
 
+require_once './controller/pelis.controller.php';
+require_once './model/pelis.model.php';
+require_once './view/pelis.view.php';
+
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+//inicio las clases
+
+
+//accion por defecto
+
+// lee la acción
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home';
+}
+
+
+
+
+
+$params = explode('/', $action);
+
+switch ($params[0]) {
+
+    
+    default: 
+        require_once "./index.php";
+        break;
+}
